3a08ecf5078f6e1912ce80fca3e139e97a8d7031
Joel Miller <joelmiller@Joels-MacBook-Pro.local>
2015-09-11 15:46:35 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Prime Weekend Challenge 1
+
+##Hey Primers!
+
+For this weekend, you will be creating an application that will process insurance claims. You will provided an array of 5 items, where you will use the constructor function to create 5 more, combining to the two to have one array of 10 items.  
+
+You will need to create two functions, one will determine what percentage (per rules below) will be covered and another will determine what that amount will be (per the visitCost object). Within your cost determining method, console out a message in the format ('Paid out ___$ for patient_name'). 
+
+After that is complete, console out the entire amount paid out.
+
+Numbers should be rounded to the nearest whole number!
+
+###Rules
+Specialist - covers 10%
+Emergency - covers 100%
+Primary Care - covers 50%	
+
+##Hard Mode
+Append to the DOM!
+
+##Pro Mode
+Style with CSS
---
+++ app.js
@@ -0,0 +1,50 @@
+var claim1 = {
+	patientName: "John Doe",
+	visitType: "Specialist",
+	visitCost: 1100
+}
+
+var claim2 = {
+	patientName: "Jane Doe",
+	visitType: "Optical",
+	visitCost: 100
+}
+
+var claim3 = {
+	patientName: "Joe Johnson",
+	visitType: "Emergency",
+	visitCost: 31000
+}
+
+var claim4 = {
+	patientName: "Sharon Smith",
+	visitType: "Emergency",
+	visitCost: 1300
+}
+
+var claim5 = {
+	patientName: "Steve Wright",
+	visitType: "Primary Care",
+	visitCost: 770
+}
+
+var initialList = [claim1, claim2, claim3, claim4, claim5]
+
+var totalPayedOut = 0;
+
+//console out "$100 paid for Steve Wright"
+
+//Optical - covers 0%
+//Specialist - covers 10%
+//Emergency - covers 100%
+//Primary Care - covers 50%	
+
+function claim(name, type, cost){
+	this.patientName = name;
+	this.visitType = type;
+	this.visitCost = cost;
+}
+
+//function to determine percent covered
+
+//function to determine amount covered
